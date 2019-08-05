f30f7cbf0d955102710e12a4bda413b9d07a4149
willyowi <wilsonowino1@gmail.com>
2019-08-05 11:20:35 +0300

    config py changed

--- config.py
+++ config.py
@@ -9,18 +9,18 @@ class Config:
 
 
 class ProdConfig(Config):
-    SECRET_KEY = os.environ.get("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI =os.environ.get('DATABASE_URL')
+#     SECRET_KEY = os.environ.get("SECRET_KEY")
+#     SQLALCHEMY_DATABASE_URI =os.environ.get('DATABASE_URL')
     
 
-class DevConfig(Config):
-    SECRET_KEY='absc'
-    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:vinceobindi1005@localhost/thinkout'
-    DEBUG = True
+# class DevConfig(Config):
+#     SECRET_KEY='absc'
+#     SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:vinceobindi1005@localhost/thinkout'
+#     DEBUG = True
 
 
-config_options = {
-'development':DevConfig,
-'production':ProdConfig
-}
+# config_options = {
+# 'development':DevConfig,
+# 'production':ProdConfig
+# }
 
