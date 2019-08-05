6c27f65ede2630f1694d4a1cbe9f345fb21d8893
willyowi <wilsonowino1@gmail.com>
2019-08-05 11:11:31 +0300

    add app folders

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,7 +1,7 @@
-# from flask import Blueprint
+from flask import Blueprint
 
-# auth = Blueprint('auth',__name__)
+auth = Blueprint('auth',__name__)
 
-# from . import views
+from . import views
 
-# from . import views,forms
+from . import views,forms
